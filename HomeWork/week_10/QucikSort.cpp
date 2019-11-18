2d3066a96b708f4a01aab074fe5b651e5dd50e9f
laowang@163.com <463970214@qq.com>
2019-11-18 19:42:58 +0800

    王啸林 week_10

--- HomeWork/week_10/QucikSort.cpp
+++ HomeWork/week_10/QucikSort.cpp
@@ -3,22 +3,51 @@
 using namespace std;
 
 //¿ìËÙÅÅĞò 
-void quickSort(int a[],int,int);
+void QuickSort(int a[],int,int);
 int partition(int a[],int,int);
+void exchange(int &a,int &b);
+void p_array(int a[],int len); 
 int main()
 {
 
+   int arr[]={1,4,5,9,2,5,3,8,6,7};
+   QuickSort(arr,0,sizeof(arr)/sizeof(arr[0])-1);
+   p_array(arr,sizeof(arr)/sizeof(arr[0]));
+   return 0;
+}
+void p_array(int a[],int len)
+{
+	for(int i=0;i<len;i++)
+	  printf("%d ",a[i]);
+	printf("\n");
 }
 void QuickSort(int a[],int p,int r)
 {
 	 if(p<r)
 	 {
-	 	q=partition(a,p,r);
-	 	QuickSort(A,p,q-1);
-	 	QuickSort(A,q+1,r);
+	 	int q=partition(a,p,r);
+	 	QuickSort(a,p,q-1);
+	 	QuickSort(a,q+1,r);
 	 }
 }
-int partition(int a[],int,int)
+int partition(int a[],int p,int r)
 {
-	
+	int x=a[r];
+	int i=p-1;
+	for(int j=p;j<r;j++)
+	{
+		if(a[j]<=x)
+		{
+			i=i+1;
+			exchange(a[i],a[j]);
+		}
+	}
+	exchange(a[i+1],a[r]);
+	return i+1;
+}
+void exchange(int &a,int &b)
+{
+	int temp=a;
+	a=b;
+	b=temp;
 }
